5bb52db838f9e120c7ac4c5343f13292618211ce
Isak Kogner <isak.kogner@gmail.com>
2018-12-04 09:39:28 +0100

    boom after 3 sec

--- space-ship/not-a-space-ship/Assets/Trap.cs
+++ space-ship/not-a-space-ship/Assets/Trap.cs
@@ -8,33 +8,44 @@ public class Trap : MonoBehaviour {
     public float liftForce;       // Force to apply when lifting the rigidbody.
     public float damping;         // Force reduction proportional to speed (reduces bouncing).
 
+    readonly float trapActivationTime = 3f;
+
     Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        StartCoroutine(ActivateTrap());
     }
 
     void FixedUpdate()
     {
+       
+    }
+
+    private IEnumerator ActivateTrap()
+    {
+        float normalizedTime = 0;
+        while (normalizedTime <= 1f)
+        {
+            normalizedTime += Time.deltaTime / trapActivationTime;
+            yield return null;
+        }
+
         // Cast a ray straight up.
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
-        Debug.DrawRay(transform.position, Vector2.up, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
         Debug.Log(hit.collider);
         // If it hits something...
         if (hit.collider != null)
         {
+            Debug.Log("BOOM");
 
-            // Calculate the distance from the surface and the "error" relative
-            // to the floating height.
             float distance = Mathf.Abs(hit.point.y - transform.position.y);
             float heightError = floatHeight - distance;
 
-            // The force is proportional to the height error, but we remove a part of it
-            // according to the object's speed.
             float force = liftForce * heightError - rb.velocity.y * damping;
 
-            // Apply the force to the rigidbody.
             rb.AddForce(Vector3.up * force);
         }
     }
