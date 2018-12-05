107eb7dcece0a3466ed8dd96cb0830ba77770ef6
Isak Kogner <isak.kogner@gmail.com>
2018-12-05 13:04:10 +0100

    move with arrows aswell

--- space-ship/not-a-space-ship/Assets/PlayerControls.cs
+++ space-ship/not-a-space-ship/Assets/PlayerControls.cs
@@ -13,18 +13,19 @@ public class PlayerControls : Photon.MonoBehaviour {
 	
 	void FixedUpdate() {
         // Movement
-        if(Input.GetKey(KeyCode.A)){
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
             transform.Translate(-speed * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(speed * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(Vector2.up * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector2.down * speed * Time.deltaTime);
         }
